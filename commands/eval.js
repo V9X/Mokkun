8570992ee85f4dd237c858f8e023920ca83673c0
kosmo <kosmoziemia@gmail.com>
2020-01-14 00:46:29 +0100

    added eval.js

---
+++ commands/eval.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra');
+const path = require('path');
+const rp = require('request-promise');
+
+module.exports = {
+    name: 'eval',
+    description: '',
+    usage: '',
+    ownerOnly: true,
+    execute(msg, args, bot)
+    {
+        let code = msg.content.slice(msg.prefix.length + this.name.length);
+        try {
+            eval(code);
+        } catch(err) {
+            msg.channel.send(`Nastąpił błąd podczas ewaluacji wyrażenia:\n\n` + err.stack, {split: true});
+        }
+    }
+}
